f9d048a5ec28135be0b4769c23c5f7558a4e3536
JoeTodes <jtwilson.home@gmail.com>
2021-02-09 14:45:25 -0700

    moved snipcart out of method, into mounted

--- src/layouts/StoreLayout.vue
+++ src/layouts/StoreLayout.vue
@@ -1,35 +1,30 @@
 <template>
-  <div class="bg-gray-200 h-screen">
-    <div>
-      <g-link class="snipcart-customer-signin">Account</g-link>
-      <g-link class="snipcart-checkout">
-        Cart
-        <span class="snipcart-total-items"></span>
-        <span class="snipcart-total-price">{{
-          this.totalPrice | formatMoney
-        }}</span>
-      </g-link>
+    <div class="bg-gray-200 h-screen">
+        <div>
+            <g-link class="snipcart-customer-signin">Account</g-link>
+            <g-link class="snipcart-checkout">
+                Cart
+                <span class="snipcart-total-items"></span>
+                <span class="snipcart-total-price">{{
+                    this.totalPrice | formatMoney
+                }}</span>
+            </g-link>
+        </div>
+        <div class="px-12">
+            <slot />
+        </div>
     </div>
-    <div class="px-12">
-      <slot />
-    </div>
-  </div>
 </template>
 
 <script>
 export default {
-  data: function() {
-    return {
-      totalPrice: 0,
-    };
-  },
-  methods: {
-    getTotalPrice: function() {
-      return Snipcart.store.getState().cart.total;
+    data: function () {
+        return {
+            totalPrice: 0,
+        };
     },
-  },
-  mounted: function() {
-    this.totalPrice = this.getTotalPrice();
-  },
+    mounted: function () {
+        this.totalPrice = Snipcart.store.getState().cart.total;
+    },
 };
 </script>
