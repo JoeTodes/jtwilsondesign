1723076ceede0dfc832bddf8950f3eceb55bd6c9
JoeTodes <jtwilson.home@gmail.com>
2021-02-08 22:31:55 -0700

    testing if snipcart api causes deploy break

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,55 +1,54 @@
 <template>
-  <SfProductCardHorizontal
-    :title="title"
-    :description="description"
-    :link="path"
-    :regularPrice="price | formatMoney"
-    :showAddToCartButton="true"
-    :wishlistIcon="false"
-    :qty="qty"
-    v-on:input="updateQuantity"
-    v-on:click:add-to-cart="addToCart"
-    ><template v-slot:image
-      ><g-link :to="path">
-        <g-image :src="imageUrl" />
-      </g-link> </template
-  ></SfProductCardHorizontal>
+    <SfProductCardHorizontal
+        :title="title"
+        :description="description"
+        :link="path"
+        :regularPrice="price | formatMoney"
+        :showAddToCartButton="true"
+        :wishlistIcon="false"
+        :qty="qty"
+        v-on:input="updateQuantity"
+        ><template v-slot:image
+            ><g-link :to="path">
+                <g-image :src="imageUrl" />
+            </g-link> </template
+    ></SfProductCardHorizontal>
 </template>
 
 <script>
 import { SfProductCardHorizontal } from "@storefront-ui/vue";
 export default {
-  data: function() {
-    return { qty: 1 };
-  },
-  props: {
-    id: String,
-    imageUrl: String,
-    description: String,
-    title: String,
-    price: Number,
-    path: String,
-  },
-  components: {
-    SfProductCardHorizontal,
-  },
-  methods: {
-    updateQuantity: function(payload) {
-      this.qty = payload;
+    data: function () {
+        return { qty: 1 };
     },
-    addToCart: async function() {
-      try {
-        await Snipcart.api.cart.items.add({
-          id: this.id,
-          name: this.title,
-          price: this.price,
-          url: this.path,
-          quantity: this.qty,
-        });
-      } catch (error) {
-        console.log(error);
-      }
+    props: {
+        id: String,
+        imageUrl: String,
+        description: String,
+        title: String,
+        price: Number,
+        path: String,
     },
-  },
+    components: {
+        SfProductCardHorizontal,
+    },
+    methods: {
+        updateQuantity: function (payload) {
+            this.qty = payload;
+        },
+        addToCart: async function () {
+            try {
+                await Snipcart.api.cart.items.add({
+                    id: this.id,
+                    name: this.title,
+                    price: this.price,
+                    url: this.path,
+                    quantity: this.qty,
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
 };
 </script>
